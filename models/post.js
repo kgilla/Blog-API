05b26e06d50bc34b53954cdb3fa43eeec06003de
Kenneth Gilmore <gilmorekenneth@gmail.com>
2020-10-13 14:23:05 -0400

    changed max restriction on posts

--- models/post.js
+++ models/post.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  blurb: { type: String, required: true, max: 100 },
+  blurb: { type: String, required: true },
   blurbImage: { type: String },
-  blurbImageAlt: {type: String},
+  blurbImageAlt: { type: String },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   author: { type: Schema.Types.ObjectId, ref: "User" },
   date: { type: Date, default: Date.now() },
